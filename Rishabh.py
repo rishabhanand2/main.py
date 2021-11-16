e436be17c6a90a308ecb4bd6239e03ce27ae8085
Rishabh anand <90849358+rishabhanand2@users.noreply.github.com>
2021-11-16 20:46:55 +0530

    Create Rishabh.py

---
+++ Rishabh.py
@@ -0,0 +1,7 @@
+
+    [
+        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇꜱ ☑️", url=f"https://t.me/The_Godfather_Network"),
+        InlineKeyboardButton(
+            text="ᴛɢɴ ᴄʜᴀᴛ", url=f"https://t.me/greatpersonxd"
+        ),
+    ],
